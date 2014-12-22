c377b08408fb36d1328a1b6e5f898a08813be300
Jacob Ashdown <jcbashdown@gmail.com>
2014-12-22 16:57:45 +0000

    move client into server assets

--- client/src/js/app.js
+++
@@ -1,5 +0,0 @@
-var backbone = require('backbone');
-
-var dashboardSlug = window.location.pathname.split('/').pop();
-
-document.getElementById('dashboard-name').innerHTML = 'This is the ' + dashboardSlug + ' dashboard';
---
+++ server/client/src/js/app.js
@@ -0,0 +1,52 @@
+var _ = require('lodash');
+var d3 = require('d3');
+var c3 = require('c3');
+var moment = require('moment');
+var format = require('./formatter')(moment);
+
+var dashboardSlug = window.location.pathname.split('/').pop();
+
+(function(d3, c3, format) {
+
+  var module_tables = document.querySelectorAll('section.module > table');
+
+  [].forEach.call(module_tables, function(table) {
+    var header_cells = table.querySelectorAll('th'),
+        rows = table.querySelectorAll('tr'),
+        axes = new Array(header_cells.length);
+
+    [].forEach.call(header_cells, function(header_cell, i) {
+      axes[i] = {
+        format_options: JSON.parse(header_cell.getAttribute('data-format')),
+        data: [header_cell.innerText],
+      }
+    });
+
+    [].forEach.call(rows, function(row) {
+      [].forEach.call(row.querySelectorAll('td'), function(cell, i) {
+        axes[i].data.push(cell.getAttribute('data-raw'));
+      });
+    });
+
+    var chart = c3.generate({
+      data: {
+        x: axes[0].data[0],
+        xFormat: "%Y-%m-%dT%H:%M:%S+00:00",
+        columns: axes.map(function(axis) { return axis.data; })
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: { format: format.bind(null, axes[0].format_options) }
+        },
+        y: {
+          tick: { format: format.bind(null, axes[1].format_options) }
+        }
+      }
+    });
+
+    table.parentNode.appendChild(chart.element);
+
+  });
+
+})(d3, c3, format);
